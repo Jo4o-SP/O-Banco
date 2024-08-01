1edccb0962a580baa2fd5df57cbdf05a21a00bdc
Jo4o-SP <joaov-alves1@hotmail.com>
2024-08-01 17:50:03 -0300

    Teste de repetição

---
+++ ContaBanco/src/TesteRepeticao.java
@@ -0,0 +1,31 @@
+/* Teste de repetições
+ * Repetição em For
+ * Repetição em While
+ * Repticção em Do While
+ */
+
+public class TesteRepeticao {
+    public static void main(String[] args) {
+
+        //Disponibilização de crédito mensal
+        /*int credito;
+        for(credito=0; credito<=12; credito++){
+            System.out.println("Crédito mensal disponibilizado: " + credito);
+        }
+        System.out.println("Crédito pendente de analise");*/
+
+        //Teste for/each
+        /*String medalhas [] ={"OURO", "PRATA", "BRONZE"};
+
+        for(String medalha : medalhas){
+            System.out.println(medalha);
+        }*/
+        //Break e Continue
+        /*for(int pensao = 1; pensao <= 5; pensao++){
+            if(pensao == 4){ 
+            continue;
+            }
+            System.out.println(pensao);
+        }*/
+    }
+}
